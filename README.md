cc71aee985281554a46133b68665ab009457d491
Daimi <wzt252951@163.com>
2024-09-25 17:53:54 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Web-React-Antd
+
+Web-React-Antd Template
+
+# Directory/File description
+
+README.js
+
+# Install dependencies
+
+yarn install
+
+# Start service
+
+yarn start
+
+# Build web application
+
+yarn build
+
+# Build output directory
+
+build
---
+++ web-renderer/src/main.tsx
@@ -0,0 +1,11 @@
+import React, {StrictMode} from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import packageJson  from '../../package.json'
+
+document.title = packageJson.name;
+ReactDOM.createRoot(document.getElementById('root')!).render(
+        <StrictMode>
+        <App />
+        </StrictMode>
+)
